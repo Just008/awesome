4c8458e7eea476941cabc0d6116fb0c113fd8318
laragh <laragh@163.com>
2019-02-25 10:22:15 +0800

    change data

--- docs/.vuepress/sidebarMap.js
+++ docs/.vuepress/sidebarMap.js
@@ -1,13 +1,26 @@
 const sidebarMap = [
-  { title: '指南', dirname: 'guide' },
-  { title: 'ES6 学习', dirname: 'web/es6' },
-  { title: 'Laravel 总结', dirname: 'backend/php/laravel' },
-  { title: 'Swoole 学习', dirname: 'backend/php/swoole' },
-  { title: '设计模式总结', dirname: 'backend/php/design-pattern' },
-  { title: 'Mysql 学习', dirname: 'backend/sql/mysql' },
-  { title: 'Docker 学习', dirname: 'linux/docker' },
-  { title: 'Linux 学习总结', dirname: 'linux' },
-  { title: '其他', dirname: 'other' }
+  { title: "指南", dirname: "guide" },
+  { title: "ES6 学习", dirname: "web/es6" },
+  { title: "Laravel 总结", dirname: "backend/php/laravel" },
+  { title: "Swoole 学习", dirname: "backend/php/swoole" },
+  { title: "设计模式总结", dirname: "backend/php/design-pattern" },
+  { title: "Mysql 学习", dirname: "backend/sql/mysql" },
+  { title: "Docker 学习", dirname: "linux/docker" },
+  { title: "Linux 学习总结", dirname: "linux" },
+  { title: "其他", dirname: "other" },
 ]
+
+const guide = [
+  { title: "指南", dirname: "guide" },
+]
+
+const web = [
+  { title: "ES6 学习", dirname: "web/es6" },
+  { title: "基础知识", dirname: "web/es6/base" },
+  { title: "ES6 学习", dirname: "web/es6" },
+  { title: "ES6 学习", dirname: "web/es6" },
+]
+
+
 
 module.exports = sidebarMap
